547d2a36f6b9cc23f4db3cae5eddf03d1ebb3e6b
EstebanWu <blueredhappyface@gmail.com>
2021-07-13 18:41:24 -0400

    Added CardViewer component

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,12 +1,69 @@
 import React from "react";
-import CardEditor from "./CardEditor";
+import "./CardViewer.css"
 
 class CardViewer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currIndex: 0,
+            display: this.props.cards[0].front,
+        }
+    }
+
+    prevCard = () => {
+        if (this.state.currIndex >= 0) {
+            this.setState({
+                currIndex: this.state.currIndex - 1,
+                display: this.props.cards[this.state.currIndex - 1].front,
+            });
+        }
+    }
+
+    nextCard = () => {
+        if (this.state.currIndex < this.props.cards.length - 1) {
+            this.setState({
+                currIndex: this.state.currIndex + 1,
+                display: this.props.cards[this.state.currIndex + 1].front,
+            });
+        }
+    }
+
+    flipCard = () => {
+        const front = this.props.cards[this.state.currIndex].front;
+        const back = this.props.cards[this.state.currIndex].back;
+        if (this.state.display === front) {
+            this.setState({ display: back });
+        } else {
+            this.setState({ display: front });
+        }
+    }
+
     render() {
+        let progress = "Progress: " + (this.state.currIndex + 1) +
+            "/" + (this.props.cards.length) + " cards";
+
         return (
             <div>
                 <h2>Card Viewer</h2>
-                <hr/>
+                <br />
+                <div>{progress}</div>
+                <br />
+                <div>Click to flip</div>
+                <div id="card" onClick={this.flipCard}>{this.state.display}</div>
+                <br />
+                <button
+                    disabled={this.state.currIndex <= 0}
+                    onClick={this.prevCard}
+                >
+                    Prev
+                </button>
+                <button
+                    disabled={this.state.currIndex >= this.props.cards.length - 1}
+                    onClick={this.nextCard}
+                >
+                    Next
+                </button>
+                <hr />
                 <button onClick={this.props.switchMode}>Go to card editor</button>
             </div>
         );
